6debdda201ae8f7b6d1638b1b1971c504e8b9a93
e.chronos@gmail.com <e.chronos@gmail.com@ae0caa49-0589-0635-f36d-ed273433bca9>
2011-06-03 16:21:16 +0000

    Ajout du recorder

---
+++ AAP/src/fr/unice/aap/recorder/AudioRecorder.java
@@ -0,0 +1,62 @@
+package fr.unice.aap.recorder;
+
+import java.io.File;
+import java.io.IOException;
+import android.media.MediaRecorder;
+import android.os.Environment;
+
+/*Classe d'enregistrement du son*/
+public class AudioRecorder {
+
+  final MediaRecorder recorder = new MediaRecorder();
+  final String path;
+
+  /**
+   * Lieu où sera placé l'enregistrement.
+   */
+  public AudioRecorder(String path) {
+    this.path = sanitizePath(path);
+  }
+
+  private String sanitizePath(String path) {
+    if (!path.startsWith("/")) {
+      path = "/" + path;
+    }
+    if (!path.contains(".")) {
+      path += ".3gp";
+    }
+    return Environment.getExternalStorageDirectory().getAbsolutePath() + path;
+  }
+
+  /**
+   * Démarre un enregistrement.
+   */
+  public void start() throws IOException {
+    String state = android.os.Environment.getExternalStorageState();
+    if(!state.equals(android.os.Environment.MEDIA_MOUNTED))  {
+        throw new IOException("SD Card is not mounted.  It is " + state + ".");
+    }
+
+    // make sure the directory we plan to store the recording in exists
+    File directory = new File(path).getParentFile();
+    if (!directory.exists() && !directory.mkdirs()) {
+      throw new IOException("Path to file could not be created.");
+    }
+
+    recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+    recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+    recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+    recorder.setOutputFile(path);
+    recorder.prepare();
+    recorder.start();
+  }
+
+  /**
+   * Arrêt de l'enregistrement
+   */
+  public void stop() throws IOException {
+    recorder.stop();
+    recorder.release();
+  }
+
+}
